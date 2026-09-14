907e200f79034a32ffff55fd26cd75d28e876861
eugeniocacahiombo <eugeniocachiombo@gmail.com>
2026-09-14 16:08:34 +0100

    perf: mini animação

--- assets/js/main.js
+++ assets/js/main.js
@@ -702,3 +702,13 @@
   }
 
 })();
+
+document.querySelectorAll('.project-card, .work-card').forEach(card => {
+  card.addEventListener('mousemove', e => {
+    const rect = card.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+    card.style.setProperty('--mouse-x', `${x}px`);
+    card.style.setProperty('--mouse-y', `${y}px`);
+  });
+});
